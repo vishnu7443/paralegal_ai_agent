78c4186f754e9c6875bae37f4f37c4ef5863ce37
Vishnu Vardhan <visshnu2007@gmail.com>
2026-06-02 11:39:58 +0400

    Vercel.json

---
+++ backend/scratch/test_thread_limit.py
@@ -0,0 +1,8 @@
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+
+print("Importing faiss...")
+import faiss
+print("faiss imported successfully")
